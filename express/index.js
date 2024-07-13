cbfb336e4eb77f6ad7e47ee0e47bfa28037bdc2d
Yash <saxenay117@gmail.com>
2024-07-13 21:45:08 +0530

    sv

--- express/index.js
+++ express/index.js
@@ -26,10 +26,6 @@ app.use('/api/users', userRoutes);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use((req, res, next) => {
-    res.status(404).send('Not Found');
-  });
-  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
