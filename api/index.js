a58657133234f09c7d8ccbcd44bd417e13fd8a37
Yash <saxenay117@gmail.com>
2024-07-14 19:34:12 +0530

    edits

--- api/index.js
+++ api/index.js
@@ -4,24 +4,23 @@ const cors = require('cors');
 const userRoutes = require('./routes/user');
 const app = express();
 
-// Middleware to parse JSON reque
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200 
   };
   
   app.use(cors(corsOptions));
 
 app.use(express.json());
-// MongoDB connection
+
 const mongoURI = `mongodb+srv://saxenay117:mongoDB2024%23@cluster1.unzxb3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-// Routes
+    
 app.use('/api/users', userRoutes);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
